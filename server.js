0eb24f93d273bdbbe7afc92a469db73a2b12d33f
Oscar Funes <me@oscarfunes.com>
2017-01-29 18:23:50 -0600

    Add /hello route

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ server.connection({
     port: 8000 
 });
 
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+
+        return reply('hello world');
+    }
+});
+
 // Start the server
 server.start((err) => {
 
