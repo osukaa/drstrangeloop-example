22e611829d5e0994006d4bfb6c770c8e097d9ede
Oscar Funes <me@oscarfunes.com>
2017-01-29 18:17:52 -0600

    Add hapi dep and server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
